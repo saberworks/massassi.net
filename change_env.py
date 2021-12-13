9b481cc7b784f833c2946fee5e819e5b42e44210
Brian E. Lozier <brian@massassi.com>
2021-12-12 16:15:26 -0800

    support multiple environments
    use `python3 change_env.py <env>` to change environments
    
    environment currently checked in:
    
    local - local development environment
        no ssl
        no certbot
        mount local massassi-django as a volume that can be edited
        run gunicorn with --reload option to reload after every code change
    
    dev - massassi.org dev server
        yes ssl + certbot
        massassi-django fs is copied in, not mounted, as in prod
        gunicorn runs as in prod
        requires user/password to see the site at all
    
    prod - massassi.net prod server
        same as dev with different domain name
        doesn't require user/password to see the site

---
+++ change_env.py
@@ -0,0 +1,58 @@
+import os.path
+import re
+import subprocess
+import sys
+
+# TODO TODO TODO
+# also change around the .env file
+
+def main():
+    cur = get_current_environment()
+    new = sys.argv[1]
+
+    if not environment_exists(new):
+        print("ERROR: `{}` environment file does not exist (create docker-compose-{}.yaml)".format(new, new))
+        sys.exit(1)
+    
+    if not env_file_exists(new):
+        print("ERROR: `{}` env file (.env-{}) does not exist".format(new, new))
+        sys.exit(1)
+
+    print("Changing environment from {} to {}".format(cur, new))
+    change_environment(new)
+
+def get_current_environment():
+    result = subprocess.run(
+        ['ls', '-l', './docker-compose.override.yml'],
+        stdout=subprocess.PIPE
+    )
+
+    env_regex = re.compile(br"docker-compose.override.yml -> docker-compose-(.*)\.yml$")
+    match = env_regex.search(result.stdout)
+
+    return match.group(1).decode() if match else ''
+
+def change_environment(new):
+    if(environment_exists(new)):
+        # Remove and replace the docker-compose.override symlink
+        override_file = 'docker-compose-{}.yml'.format(new)
+
+        subprocess.run(['rm', 'docker-compose.override.yml'], check=False)
+
+        subprocess.run(
+            ['ln', '-s', override_file, 'docker-compose.override.yml'],
+            check=True
+        )
+
+        # Remove and replace the .env symlink
+        subprocess.run(['rm', '.env'], check=False)
+        subprocess.run(['ln', '-s', '.env-{}'.format(new), '.env'], check=True)
+
+def environment_exists(env_name):
+    return os.path.exists('docker-compose-{}.yml'.format(env_name))
+
+def env_file_exists(env_name):
+    return os.path.exists('.env-{}'.format(env_name))
+
+if __name__ == '__main__':
+    main()
